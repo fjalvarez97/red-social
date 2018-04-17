997bb422b36a8a7b38550b1287c6b5ce195c1a8d
fjalvarez97 <franjalvarez955@gmail.com>
2018-04-18 00:55:07 +0200

    Implementada la clase muro que tiene colecciones de entradas de texto e/o imagen

---
+++ Muro.java
@@ -0,0 +1,71 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+/**
+ * Esta clase simula un muro de publicaciones de texto y fotos
+ * 
+ * @FranAlvarez 
+ * @Version 1.0
+ */
+public class Muro
+{
+    private ArrayList<EntradaTexto> mensajes;
+    private ArrayList<EntradaFoto> fotos;
+    /**
+     * Constructor for objects of class Muro
+     */
+    public Muro()
+    {
+        mensajes = new ArrayList<>();
+        fotos = new ArrayList<>();
+    }
+
+    /**
+     * Este metodo anade una entrada de texto al muro
+     * @param la entrada de texto que vas a anadir
+     */
+    public void addEntradaTexto(EntradaTexto entradaTexto)
+    {
+        mensajes.add(entradaTexto);
+    }
+
+    /**
+     * Este metodo anade una entrade de foto/imagen al muro
+     * @param la entrada de imagen que vas a anadir
+     */
+    public void addEntradaFoto(EntradaFoto entradaFoto)
+    {
+        fotos.add(entradaFoto);
+    }
+
+    /**
+     * @return la informacion completa de la entrada
+     */
+    public String toString()
+    {
+        String textoADevolver = "";
+        if (mensajes.size()>0){
+            System.out.println("Entradas de texto:");
+            for (EntradaTexto entradaTexto : mensajes)
+            {
+                textoADevolver += entradaTexto;
+                System.out.println(entradaTexto);
+            }
+        }
+        else{
+            System.out.println("No hay ninguna entrada de texto\n");
+        }
+        System.out.println("\n-------------------------------------\n");
+        if (fotos.size()>0){
+            System.out.println("\nEntradas de imagen:");
+            for (EntradaFoto entradaFoto : fotos)
+            {
+                textoADevolver += entradaFoto;
+                System.out.println(entradaFoto);
+            }
+        }
+        else{
+            System.out.println("No hay ninguna entrada de imagen");
+        }
+        return textoADevolver;
+    }
+}
